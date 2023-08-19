8b042636ca77485c53313240cd5a017a230dfd39
Mitchel <324712@student.mboutrecht.nl>
2023-08-19 13:03:43 +0200

    add(shared) added config for webhook and inventory

---
+++ shared/main.lua
@@ -0,0 +1,7 @@
+QBCore = exports['qb-core']:GetCoreObject()
+
+Config = {}
+
+Config.Webhook = "https://discord.com/api/webhooks/1142407821544132699/4sLpuDnw6xftAhiHCHKmDZX0Y_vPuG7Njc2Ak1cCHXUxtTweNpHkcZ5LKbcC1P86mKYW"
+
+Config.Inventory = "ox" -- ox or qb
